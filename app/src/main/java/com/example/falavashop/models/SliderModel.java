891e40dfcb79cf8534b3bdc4a5f3c04488a19f51
An Do <s3878698@rmit.edu.vn>
2022-12-12 17:17:34 +0700

    add slider view

---
+++ app/src/main/java/com/example/falavashop/models/SliderModel.java
@@ -0,0 +1,22 @@
+package com.example.falavashop.models;
+
+public class SliderModel {
+
+    private String imgUrl;
+
+    // Constructor method.
+    public SliderModel(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    // Getter method
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    // Setter method
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+
+    }
+}
