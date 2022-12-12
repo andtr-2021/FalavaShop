bd7fb0e175e54e0da6ffa2fdce5b0bd3718f9266
dathuynh111 <huynhdactandat@gmail.com>
2022-12-12 15:37:45 +0700

    Add Several Thing (Stable)

--- app/src/main/java/com/example/falavashop/MainActivity.java
+++ app/src/main/java/com/example/falavashop/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.falavashop.Base.BaseApp;
+
+public class MainActivity extends BaseApp {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayout() { return R.layout.activity_main;}
+
+    @Override
+    public void initView() {
+
     }
+
+    @Override
+    public void setEvents() {
+
+    }
+
+    @Override
+    public void setData() {
+
+    }
+
 }
