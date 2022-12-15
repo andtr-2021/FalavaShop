c3ab7a0685342f11ff06951ddbc227b48325b3e0
dathuynh111 <huynhdactandat@gmail.com>
2022-12-15 14:33:34 +0700

    Drawer

--- app/src/main/java/com/example/falavashop/MainActivity.java
+++ app/src/main/java/com/example/falavashop/MainActivity.java
@@ -9,17 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import com.bumptech.glide.Glide;
+import com.example.falavashop.UI.Activity.SearchActivity;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     ViewFlipper viewFlipper;
-
+    NavigationView navigationView;
+    ListView listViewTrangChinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
             Glide.with(getApplicationContext()).load(ads.get(i)).into(imageView);
             imageView.setScaleType(ImageView.ScaleType.FIT_XY);
             viewFlipper.addView(imageView);
-
         }
         viewFlipper.setFlipInterval(3000);
         viewFlipper.setAutoStart(true);
@@ -64,21 +69,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void getEventClick() {
+        listViewTrangChinh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        Intent trangchu = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(trangchu);
+                        break;
+                    case 1:
+                        Intent dienthoai = new Intent(getApplicationContext(), SearchActivity.class);
+                        dienthoai.putExtra("loai", 1);
+                        startActivity(dienthoai);
+                        break;
+                }
+            }
+        });
+    }
+
     private boolean isConnected(Context context){
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if(wifi != null && wifi.isConnected() || (mobile !=null && mobile.isConnected())){
-            return true;
-        }else{
-            return false;
-        }
+        return wifi != null && wifi.isConnected() || (mobile != null && mobile.isConnected());
     }
 
     private void ActionBar() {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
-/*        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);*/
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,9 +111,12 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.tool_bar_controller);
         viewFlipper = findViewById(R.id.vewlipper);
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationview);
+        listViewTrangChinh = findViewById(R.id.listviewtrangchinh);
 
         if(isConnected(this)){
             ActionViewFlipper();
+            getEventClick();
         }else{
             Toast.makeText(getApplicationContext(), "Không có Internet, vui lòng kết nối Internet", Toast.LENGTH_LONG).show();
         }
